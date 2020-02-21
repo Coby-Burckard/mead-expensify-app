bc2f3443d02c44cd38eba9d36418410f9a8e6120
Coby-Burckard <51294008+Coby-Burckard@users.noreply.github.com>
2020-02-20 22:32:37 -0600

    fixed defined pluggin

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,8 @@ if (process.env.NODE_ENV === 'test') {
   require('dotenv').config({ path: '.env.development'})
 }
 
+console.log(process.env)
+
 module.exports = (env, argv) => {
 
   const isProduction = env === 'production'
@@ -56,13 +58,12 @@ module.exports = (env, argv) => {
       new webpack.DefinePlugin({
         'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
         'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-        'process.env.FIREBASE_DATABASE_URL': JSON.stringify("process.env.FIREBASE_DATABASE_URL"),
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
         'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
         'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
         'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
         'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
         'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID),
-        'process.env.penis_penis_penis' : 'penis'
       })
     ],
     devtool: isProduction ? "source-map" : "inline-source-map",
